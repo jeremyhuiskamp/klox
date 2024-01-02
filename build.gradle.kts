4e4c1233f32325ba522c568b270390dbfa3bd172
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2024-01-02 16:14:48 +0100

    configure gradle to run as application

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.21"
+    application
 }
 
 group = "ca.kamper"
@@ -16,6 +17,16 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(17)
+}
+
+application {
+    mainClass = "ca.kamper.klox.LoxKt"
+}
+
+// https://blog.thecodewhisperer.com/permalink/stdin-gradle-kotlin-dsl
+tasks.run<JavaExec> {
+    standardInput = System.`in`
 }
